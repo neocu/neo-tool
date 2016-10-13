8f1b1aeb31d93ac804fa59e3143db7198328b6e9
zhouyong <zhouyong@chinac.com>
2016-10-13 22:16:33 +0800

    test

--- test/index.test.js
+++ test/index.test.js
@@ -2,12 +2,14 @@
  * Created by ssehacker on 2016/10/13.
  */
 // 支持ES6 语法
-import assert from 'assert';
+const assert = require('assert');
+// import assert from 'assert';
+const test = require('../lib');
 
 describe('Demo test', () => {
   describe('#indexOf()', () => {
     it('should return -1 when the value is not present', () => {
-      assert.equal(-1, [1, 2, 3].indexOf(4));
+      assert.equal(test(), 'hello');
     });
   });
 });
