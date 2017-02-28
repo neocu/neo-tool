9f71d5435b3233508059f77fcc681c517aca8901
zhouyong <zhouyong@chinac.com>
2017-02-28 17:23:10 +0800

    support init component.

---
+++ template/init/README.md
@@ -0,0 +1,2 @@
+# {moduleName}
+wisite {name}
---
+++ template/init/src/index.js
@@ -0,0 +1,12 @@
+class {moduleName} extends React.Component {
+  render() {
+
+    return (
+      <div className="neo-{name}">
+        {moduleName}
+      </div>
+    );
+  }
+}
+
+export default {moduleName};
